907bab28fe2ad2f458df9aa3eecdd1862a6c7d2a
Travis Lee <travisslee223@gmail.com>
2020-09-24 18:15:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   # write code here
   attr_accessor :length1, :length2, :length3
   
-  def initialize(attributes)
+  def initialize(length1:, :length2, :length3)
     attributes.each {|key, value| self.send(("#{key}="), value)} 
   end
   
